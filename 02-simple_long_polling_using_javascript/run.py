69b0c43c2a2e89844fd20224d53074d3c77d53a4
Aaron Sinnott <aaronsnig@gmail.com>
2017-12-07 08:33:10 +0000

    Initial commit

---
+++ 02-simple_long_polling_using_javascript/run.py
@@ -0,0 +1,45 @@
+import os
+from datetime import datetime
+from flask import Flask, redirect, render_template
+
+app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(message_dict)
+
+
+def get_all_messages():
+    """Get al of the messages and separate them by a `br`"""
+    return messages
+
+
+@app.route('/', methods=["GET", "POST"])
+def index():
+    """Main page with instructions"""
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.writelines(request.form["username"] + "\n")
+        return redirect(username)
+    return render_template("index.html")
+
+
+@app.route('/<username>')
+def user(username):
+    """Display chat messages"""
+    messages = get_all_messages()
+    return render_template("chat.html",
+                            username=username, chat_messages=messages)
+
+
+@app.route('/<username/<message>')
+def send_message(username, message):
+    """Create a new message and redirect back to the chat page"""
+    add_messages(username, message)
+    return redirect(username)
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
